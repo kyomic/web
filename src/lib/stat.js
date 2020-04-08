457f06fd8687f9c07875180d2a36fce41bda5d66
wangxk <wangxk@fun.tv>
2020-04-08 15:46:23 +0800

    {feat}添加fps监测工具

--- src/lib/stat.js
+++ src/lib/stat.js
@@ -8,52 +8,82 @@ class Stat{
 	}
 
 	constructor( wrapper ){
+		this.width = 100;
+		this.height = 30;
 		this.wrapper = document.createElement('div');
 		this.wrapper.className = 'k-stat';
+		this.wrapper.style.cssText = "position:fixed;color:white;display:inline-block;left:0;bottom:0;z-index:888;"
 		wrapper.appendChild( this.wrapper );
 		var canvas = document.createElement('canvas');
+		this.canvas_tmp = document.createElement('canvas');
+		canvas.width = this.canvas_tmp.width = this.width;
+		canvas.height = this.canvas_tmp.height = this.height;
+
+		this.infowrap = document.createElement("div");
+		this.wrapper.appendChild( this.infowrap );
+		this.infowrap.style.cssText = "position:absolute;left:0;bottom:0;color:white;padding:4px 0;font-family: Verdana;font-size:9px;-webkit-transform: scale(0.80);";
+
+		this.info_fps = document.createElement("div");
+		this.infowrap.appendChild( this.info_fps)
+
 		canvas.setAttribute("id","canvas")
-		canvas.style.cssText = "border:1px solid #00cc00"
+		canvas.style.cssText = "background:black;"
+		this.ctx_tmp = this.canvas_tmp.getContext('2d');
+
 		this.width = canvas.width;
 		this.height = canvas.height;
 		this.wrapper.appendChild( canvas )
 		this.canvas = canvas;
 		this.fpsCanvas = canvas.getContext('2d');
-
-		
-		console.log("状态模块", this.wrapper, this.width,this.height)
-
 		this.times = []; // 存储当前的时间数组
 		this.fps = 0;
 		this.updateFps();
 		this.fpsIdx = 0;
-		let inter = setInterval(_=>{
-			let ctx = this.fpsCanvas, fps = this.fps;
-			ctx.drawImage( this.canvas,1,0 )
-			ctx.clearRect(0,0,this.width, this.height)
-			ctx.save()
-			ctx.fillStyle="#FF0000";
-			let val = Math.floor(Math.random()*20)+20
-			ctx.fillRect(0, val, 10, 10);
-			ctx.fill()
-			ctx.restore();
-			
-			//ctx.restore();
-			if( this.fpsIdx > this.width ){
-				console.log('end')
-				//clearInterval( inter )
-
-				//ctx.translate(10,0);
-			}
-			//clearInterval( inter )
-			this.fpsIdx +=1;
-		},1000)
+		this.oldfps = 0;
+		let self = this;
+		setInterval(function(){
+			self.drawFps( self.fps );
+		},300)
 	}
 
+	drawFps( fps ){
+		let ctx = this.fpsCanvas
+		let ctx_tmp = this.ctx_tmp, canvas_tmp = this.canvas_tmp;
+		let val = 0;
+		let w = this.width;
+		let h = this.height;
+		let r = 0
+		let g = 204
+		let b = 0
+		let color = "rgb("+r+","+g+","+b+")";
+		let oldy = this.oldfps;
+		val = h - (h * fps/100);
+		if( !oldy ){
+			oldy = val + 1;
+		}
+		if( this.fpsIdx >= w ){				
+			ctx_tmp.clearRect(0,0,w,h);
+			ctx_tmp.drawImage( this.canvas, 0, 0 );
+			ctx.clearRect(0,0, w,h );
+			ctx.drawImage( canvas_tmp, 0, 0, w, h, -1, 0, w, h );				
+			ctx.fillStyle = color;
+			ctx.fillRect(w-1, Math.min(oldy,val), 1,Math.max( Math.abs(oldy-val), 1));			
+		}else{
+			ctx.save();			
+			ctx.translate(this.fpsIdx,0 )
+			ctx.fillStyle= color
+			ctx.fillRect(0, Math.min(oldy,val), 1, Math.max( Math.abs(oldy-val), 1));
+			ctx.restore();
+		}
+		this.oldfps = val;
+		this.fpsIdx +=1;
+
+		this.info_fps.innerHTML = "FPS：" + this.fps
+	}
 	updateFps(){
 		let times = this.times;
-		let fps = this.fps;
-		
+		let fps = this.fps;		
+		let self = this;
 		window.requestAnimationFrame(() => {
 			const now = performance.now(); // 使用performance.now()能获取更高的精度
 			while (times.length > 0 && times[0] <= now - 1000) {
@@ -61,11 +91,11 @@ class Stat{
 			}			
 			times.push(now);
 			fps = times.length;
-			this.fps =  fps;			
+			this.fps =  fps;
+			
 			this.updateFps();
-		});
+		});		
 	}
-
 
 }
 
